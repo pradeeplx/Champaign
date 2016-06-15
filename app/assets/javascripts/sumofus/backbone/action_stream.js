8e1d7b392fce6c56733debcc4fe3b0d1d3400aa5
Neal Donnelly <NealJMD@gmail.com>
2016-06-15 15:09:05 -0600

    connect homepage action stream to web socket

--- app/assets/javascripts/sumofus/backbone/action_stream.js
+++ app/assets/javascripts/sumofus/backbone/action_stream.js
@@ -1,15 +1,34 @@
 const ActionStream = Backbone.View.extend({
 
   el: '.action-stream',
+  ACTION_SOURCE: 'http://live-actions-production.cwctan5wfu.us-west-2.elasticbeanstalk.com',
+  ANIMATION_SECONDS: 0.8,
+  MAX_ACTION_ELS: 20,
 
   initialize() {
     window.setInterval(this.updateTimestamps.bind(this), 1000);
+    window.setInterval(this.renderActions.bind(this), this.ANIMATION_SECONDS*1000);
     this.actionsContainer = this.$('.action-stream__actions');
-    this.addAction({type: 'signature', name: 'Chester Watson'});
-    window.setTimeout(() => {this.addAction({type: 'donation', name: 'Jordy Wabbit'})}, 1200);
-    window.setTimeout(() => {this.addAction({type: 'donation', name: 'Michael G.'})}, 2400);
-    window.setTimeout(() => {this.addAction({type: 'donation', name: 'Michael J.'})}, 4000);
-    window.setTimeout(() => {this.addAction({type: 'signature', name: 'Werner Van Braun'})}, 6000);
+    this.actionQueue = [];
+    this.socket = io(this.ACTION_SOURCE);
+    this.socket.on('actions', this.handleMessage.bind(this));
+  },
+
+  handleMessage(msg) {
+    console.log(JSON.parse(msg));
+    this.actionQueue.push(JSON.parse(msg));
+  },
+
+  formatName(firstName, lastName) {
+    let formattedFirstName = this.capitalize(firstName);
+    if(lastName.length === 0){
+      return formattedFirstName;
+    }
+    return `${formattedFirstName} ${lastName.slice(0, 1).toUpperCase()}.`
+  },
+
+  capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
   },
 
   updateTimestamps() {
@@ -21,22 +40,37 @@ const ActionStream = Backbone.View.extend({
     });
   },
 
-  addAction(action) {
-    this.actionsContainer.prepend(this.template(action.type, action.name, 1));
+  // rather than render the actions as they arrive, we batch them and add
+  // them every ANIMATION_SECONDS because the CSS transition gets stopped
+  // when we add new actions.
+  renderActions() {
+    if(this.actionQueue.length === 0) { return; }
 
-    // additions need to be batched in case the time difference between additions
-    // is less than the time it takes to animate one onto the screen
+    // add the elements to the DOM
+    let totalWidth = 0
+    for (var ii = 0; ii < this.actionQueue.length; ii++) {
+      let action = this.actionQueue[ii];
+      let name = this.formatName(action.first_name, action.last_name);
+      this.actionsContainer.prepend(this.template(action.type, name, 1));
+      totalWidth += this.actionsContainer.children().first().outerWidth();
+    }
+    this.actionQueue = [];
+
+    // prune excessive elements no longer on screen
+    this.actionsContainer.children().slice(this.MAX_ACTION_ELS).remove();
+
+    // animate the DOM so new elements appear to slide in
     this.actionsContainer.css('transition', 'left 0s');
-    this.actionsContainer.css('left', `-${this.actionsContainer.children().first().outerWidth()}px`);
+    this.actionsContainer.css('left', `-${totalWidth}px`);
     window.setTimeout(()=> {
-      this.actionsContainer.css('transition', 'left 1s ease-in-out');
+      this.actionsContainer.css('transition', `left ${this.ANIMATION_SECONDS}s ease-in-out`);
       this.actionsContainer.css('left', '0');
     }, 32);
   },
 
   template(action_type, name, seconds_ago) {
-    if(action_type !== 'signature' && action_type !== 'donation') {
-      console.error(`Invalid action_type '${action_type}'. Expected 'signature' or 'donation'.`);
+    if(action_type !== 'petition' && action_type !== 'donation') {
+      console.error(`Invalid action_type '${action_type}'. Expected 'petition' or 'donation'.`);
       return;
     }
     let actionTranslation = `homepage.action_stream.${action_type}`
